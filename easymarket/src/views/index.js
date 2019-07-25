a316c9053e481030cf2e960538a17de8e4ec3fb8
hanna0908 <h5139706n@163.com>
2019-07-25 14:48:40 +0800

    基本框架

--- easymarket/src/views/index.js
+++ easymarket/src/views/index.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react'
 import Home from './page/home';
-<<<<<<< HEAD
-
-import Classify from "./page/classify"
-import {
-    Route,
-    BrowserRouter as Router,
-    Redirect,
-    Switch
-} from "react-router-dom";
-=======
-import { BrowserRouter as Router, Switch, Route ,Redirect} from "react-router-dom";
->>>>>>> 3d7ed604404b45f8e747b9423f4792097c26e9ca
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 // import { inject, observer } from "mobx-react"
 
 class App extends Component {
@@ -19,12 +8,8 @@ class App extends Component {
         return (
             <Router>
                 <Switch>
-<<<<<<< HEAD
-                    <Route path='/' component={Home} />
-=======
                     <Route path="/" component={Home} />
                     <Redirect from='/' to='/home' />
->>>>>>> 3d7ed604404b45f8e747b9423f4792097c26e9ca
                 </Switch>
             </Router>
         )
