87ce18d976d6f618da5c5febe79ef93603d1244f
hanna0908 <h5139706n@163.com>
2019-08-01 01:30:56 +0800

    我的收藏切换详情

--- easymarket/src/components/hotGoods/index.js
+++ easymarket/src/components/hotGoods/index.js
@@ -5,12 +5,13 @@ class HotGoods extends Component {
         let { item } = this.props
         return (
             <>
-                <a className="hotGoodsItem" href={`/goods/${item.id}`} >
+                <a className="hotGoodsItem" href={`/goods/${item.id&&item.value_id}`} >
                     <img className="imgLazyload" src={item.list_pic_url} alt="imgLazyLoad" />
                     <div className="hotGoodsInfos">
                         <div className="hotGoodsName">{item.name}</div>
                         <div className="hotGoodsInfo">{item.goods_brief}</div>
                         <div className="hotGoodsPrice">￥{item.retail_price}</div>
+                        < div className="colse show">删除</div>
                     </div>
                 </a>
             </>
